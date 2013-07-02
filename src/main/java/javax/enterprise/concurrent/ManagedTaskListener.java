e3bcc380634a5060b135c6c8e7a23b6c18032bd7
emmartins <emartins@redhat.com>
2013-07-02 07:53:43 +0100

    JBEE-122: update to match spec's final release. No javadocs or cosmetic changes since it's not yet decided if javadocs should be copied or not.

--- src/main/java/javax/enterprise/concurrent/ManagedTaskListener.java
+++ src/main/java/javax/enterprise/concurrent/ManagedTaskListener.java
@@ -5,11 +5,11 @@ import java.util.concurrent.Future;
 /**
  */
 public interface ManagedTaskListener {
-    void taskAborted(Future<?> future, ManagedExecutorService executor, Throwable exception);
+    void taskAborted(Future<?> future, ManagedExecutorService executor, Object task, Throwable exception);
 
-    void taskDone(Future<?> future, ManagedExecutorService executor, Throwable exception);
+    void taskDone(Future<?> future, ManagedExecutorService executor, Object task, Throwable exception);
 
-    void taskStarting(Future<?> future, ManagedExecutorService executor);
+    void taskStarting(Future<?> future, ManagedExecutorService executor, Object task);
 
-    void taskSubmitted(Future<?> future, ManagedExecutorService executor);
+    void taskSubmitted(Future<?> future, ManagedExecutorService executor, Object task);
 }
